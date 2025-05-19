e33083345dcecab99526df5beba77eef355a0d78
Avner Levy <avner.levi@codere.com>
2025-05-19 16:14:36 +0300

    p

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
